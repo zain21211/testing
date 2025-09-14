ec20e580a218dc9e3f902ad8fc9b940694ea9f92
zainulabdin <zainalikhan338@gmail.com>
2025-09-14 18:14:44 +0500

    payment voucher updates

--- frontend/src/PaymentVoucher.jsx
+++ frontend/src/PaymentVoucher.jsx
@@ -46,7 +46,8 @@ const PaymentVoucher = () => {
             creditCust,
             onSubmissionSuccess: () => {
                 clearForm();
-                clearCustomerSelections();
+                descRef?.current?.focus();
+                // clearCustomerSelections();
             },
         });
     };

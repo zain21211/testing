857015bdcff48b860f5ab47533162f8f4bcd2a34
zainulabdin <zainalip605@gmail.com>
2026-05-11 22:33:34 +0500

    Web app

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -15,7 +15,7 @@ setupAxios();
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').then((reg) => {
+    navigator.serviceWorker.register('/sw66767.js').then((reg) => {
       console.log('SW registered:', reg);
     }).catch((err) => {
       console.error('SW registration failed:', err);
